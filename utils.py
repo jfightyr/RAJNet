8c3659b37cde285571b2ca20e5a40dc1a8617fda
jack <jiehuihuang1107@163.com>
2023-04-20 04:34:32 +0000

    dataset changed

--- utils.py
+++ utils.py
@@ -10,7 +10,6 @@ import torchvision
 import torchvision.transforms as transforms
 import torchvision.models as models
 import torchvision.utils as vutils
-# from graphviz import Digraph
 
 import logging
 from collections import OrderedDict
@@ -403,76 +402,6 @@ def save_single_tensor(tensor, filename):
     assert len(image.shape) == 2
     image = np.uint8(image * 255)
     flag = cv2.imwrite(filename, image)
-
-# def log_parse(log_path, keyword="val_dice", index="epoch"):
-#     s_keyword = keyword + ":\d\.\d+"
-#     s_index = index + ":\d+"
-#     keyword_list = []
-
-#     with open(log_path, 'r') as f:
-#         log = f.readlines()
-#         for record in log:
-#             kw = re.search(s_keyword, record)
-#             if kw is not None:
-#                 idx = re.search(s_index, record).span()
-#                 idx_str = record[idx[0]:idx[1]]
-#                 kw_pos = kw.span()
-#                 kw_str = record[kw_pos[0]:kw_pos[1]]
-#                 keyword_list.append('-'.join((kw_str,idx_str)))
-
-#     best_result = sorted(keyword_list)[::-1][0]
-
-#     return best_result
-
-# def make_dot(var, params=None):
-#   """ Produces Graphviz representation of PyTorch autograd graph
-#   Blue nodes are the Variables that require grad, orange are Tensors
-#   saved for backward in torch.autograd.Function
-#   Args:
-#     var: output Variable
-#     params: dict of (name, Variable) to add names to node that
-#       require grad (TODO: make optional)
-#   """
-#   if params is not None:
-#     assert isinstance(params.values()[0], Variable)
-#     param_map = {id(v): k for k, v in params.items()}
-  
-#   node_attr = dict(style='filled',
-#            shape='box',
-#            align='left',
-#            fontsize='12',
-#            ranksep='0.1',
-#            height='0.2')
-#   dot = Digraph(node_attr=node_attr, graph_attr=dict(size="12,12"))
-#   seen = set()
-  
-#   def size_to_str(size):
-#     return '('+(', ').join(['%d' % v for v in size])+')'
-  
-#   def add_nodes(var):
-#     if var not in seen:
-#       if torch.is_tensor(var):
-#         dot.node(str(id(var)), size_to_str(var.size()), fillcolor='orange')
-#       elif hasattr(var, 'variable'):
-#         u = var.variable
-#         name = param_map[id(u)] if params is not None else ''
-#         node_name = '%s\n %s' % (name, size_to_str(u.size()))
-#         dot.node(str(id(var)), node_name, fillcolor='lightblue')
-#       else:
-#         dot.node(str(id(var)), str(type(var).__name__))
-#       seen.add(var)
-#       if hasattr(var, 'next_functions'):
-#         for u in var.next_functions:
-#           if u[0] is not None:
-#             dot.edge(str(id(u[0])), str(id(var)))
-#             add_nodes(u[0])
-#       if hasattr(var, 'saved_tensors'):
-#         for t in var.saved_tensors:
-#           dot.edge(str(id(t)), str(id(var)))
-#           add_nodes(t)
-#   add_nodes(var.grad_fn)
-
-#   return dot
 
 def create_dir(dir_list):
     assert  isinstance(dir_list, list) == True
